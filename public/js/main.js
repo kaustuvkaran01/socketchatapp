ecc043b45d472c8a148bbad5947d4504b63eb1ca
kaustuvkaran01 <kaustuvkaran01@gmail.com>
2021-07-25 15:50:47 +0530

    temp commit

---
+++ public/js/main.js
@@ -0,0 +1,49 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
+//Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
+
+const socket = io();
+
+//Join Chatroom
+socket.emit('joinRoom',{
+    username,
+    room
+})
+
+
+//Message from server
+socket.on('message', message => {
+    console.log(message);
+    outputMessage(message);
+
+    //Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+//Message submit
+chatForm.addEventListener('submit', e=>{
+    e.preventDefault();
+
+    //Get message text
+    const msg = e.target.elements.msg.value;
+        console.log(msg);
+
+    socket.emit('chatMessage',msg);
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+});
+
+//Output message to DOM
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
